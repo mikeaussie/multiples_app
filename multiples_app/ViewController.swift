21a4dc6d14b2196eb5bda9f4f8df52c33908609c
mikeaussie <michael.pyatin@me.com>
2016-03-09 09:10:47 +1000

    Adding the last version

--- multiples_app/ViewController.swift
+++ multiples_app/ViewController.swift
@@ -10,16 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var multiplesLogoTxt: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var multipleBoxTxt: UITextField!
+    @IBOutlet weak var pressAddToAddLbl: UILabel!
+    
+    @IBOutlet weak var outcomeLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    //properties
+    var maxRepetitions = 10
+    var multipleNum = 0
+    var multipleSum = 0
+    
+    var currentClicks = 0
+    var numFirst = 0
+    
+    @IBAction func onPlayBtn(sender: UIButton) {
+        
+        if multipleBoxTxt.text != nil && multipleBoxTxt.text != "" {
+        multiplesLogoTxt.hidden = true
+        playBtn.hidden = true
+        multipleBoxTxt.hidden = true
+        
+        pressAddToAddLbl.hidden = false
+        outcomeLbl.hidden = false
+        addBtn.hidden = false
+            
+            multipleNum = Int(multipleBoxTxt.text!)!
+            updateOutcomeLbl()
+        }
+        
     }
+    
+    @IBAction func onAddBtn(sender: UIButton) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        currentClicks+=1
+        
+        if isGameOver() {
+            restartGame()
+            
+        } else {
+        
+            updateOutcomeLbl()
+        
     }
+    }
+    
+    func updateOutcomeLbl() {
+        
+        numFirst = currentClicks * multipleNum
+        multipleSum = numFirst + multipleNum
+        
+        outcomeLbl.text = "\(numFirst) + \(multipleNum) = \(multipleSum)"
+        
+    }
+    
 
+    
+    func restartGame() {
+        
+        multipleBoxTxt.text = ""
+        outcomeLbl.text = ""
+        
+        numFirst = 0
+       multipleNum = 0
+    //   multipleSum = 0
+       currentClicks = 0
+        
+        
+        multiplesLogoTxt.hidden = false
+        playBtn.hidden = false
+        multipleBoxTxt.hidden = false
+        
+        pressAddToAddLbl.hidden = true
+        outcomeLbl.hidden = true
+        addBtn.hidden = true
+    }
+    
+    
+    func isGameOver () -> Bool {
+        if currentClicks >= maxRepetitions {
+            return true
+        } else {
+            return false
+        }
 
 }
-
+}
